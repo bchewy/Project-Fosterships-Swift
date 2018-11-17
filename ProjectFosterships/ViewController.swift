06fd5d01e27908b72e34a5e93f9aa77a3ce0f98a
brianchew <brian@bchewy.com>
2018-11-18 03:55:45 +0800

    Initial Commit

---
+++ ProjectFosterships/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectFosterships
+//
+//  Created by Brian Chew on 18/11/18.
+//  Copyright © 2018 Brian Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
